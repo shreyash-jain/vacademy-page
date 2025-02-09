ef9742089c77fbb82d6808809754932778e11f7d
Shreyash <shreyashjain1996@gmail.com>
2025-02-09 14:28:04 +0530

    added path to assess and lms

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,35 @@
 /** @type {import('next').NextConfig} */
+
 const nextConfig = {
   reactStrictMode: true,
+  async rewrites() {
+    return {
+      beforeFiles: [
+        // if the host is `app.acme.com`,
+        // this rewrite will be applied
+        {
+          source: '/:path*',
+          has: [
+            {
+              type: 'host',
+              value: 'assess.vacademy.io',
+            },
+          ],
+          destination: '/assess/:path*',
+        },
+        {
+          source: '/:path*',
+          has: [
+            {
+              type: 'host',
+              value: 'lms.vacademy.io',
+            },
+          ],
+          destination: '/lms/:path*',
+        }
+      ],
+    };
+  },
 };
 
 export default nextConfig;
